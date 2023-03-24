4c53d1a4ce1b2c2c5dc32c77f113b18d8013590b
duyKhanh026 <117454177+duyKhanh026@users.noreply.github.com>
2023-03-24 20:15:49 +0700

    Delete src/main/java directory

--- src/main/java/MyTable/StatusType.java
+++
@@ -1,5 +0,0 @@
-package MyTable;
-
-public enum StatusType {
-	LÀM_VIỆC, TẠM_NGHỈ, NGHỈ_LUÔN
-}
--- src/main/java/event/EventMenuSelected.java
+++
@@ -1,6 +0,0 @@
-package event;
-
-public interface EventMenuSelected {
-
-    public void menuSelected(int index, EventMenuCallBack callBack);
-}
